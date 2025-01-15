48ee1d19a721043cdef7176b066053dc8c23f591
alexandrofuchs <aalexandroffuchs@gmail.com>
2025-01-15 06:48:31 -0300

    configuration : [app launcer icon, app splashscreen]

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fav_movies/core/splashscreen/app_splash_screen.dart';
 import 'package:fav_movies/load_environment.dart';
 import 'package:fav_movies/data/local_database/load_local_db.dart';
 import 'package:fav_movies/root/app_module.dart';
@@ -10,10 +11,15 @@ import 'package:flutter_modular/flutter_modular.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  runApp(loadSplashScreen());
+  
   await Future.wait([
     loadAppEnvironment(),
     loadLocalDb(),
+    Future.delayed(const Duration(milliseconds: 2000))
   ]);
+
+  
 
   Animate.restartOnHotReload = true;
 
